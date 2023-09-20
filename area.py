2f06374332e04dc90a6c58281cea2afd2495e17a
pariagarwal16 <145346492+pariagarwal16@users.noreply.github.com>
2023-09-20 11:26:06 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 18 16:33:11 2023
+
+@author: pari1
+"""
+
+#Accepts the radius of the circle from the user and computes area
+r = float(input("Input the radius of circle : "))
+print ("The area of the circle with radius " + str(r) + "is:" + str(3.14285* r**2))
---
+++ filename.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 20 11:09:44 2023
+
+@author: pari1
+"""
+
+filename = input("Input the filename:")
+f_extns = filename.split(".")
+print("The extension of the file is: " + repr(f_extns[-1]))
+
